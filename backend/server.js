e3c73bfe880a9dedb3b1bfc09dc0ddbd05e96d78
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-03-10 13:11:30 +0530

    created a route for respond to a get request

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,18 @@ const express = require('express')
 
 
 
-
+//express app
 const app = express() //creates the express app and stored in the app constant
 
-//listen for requests
+//creates a route for react to a request
+app.get('/', (req, res) => { //responds to the get request
+    res.json({mssg: 'Welcome to the app'})
+})
 
+
+
+
+//listen for requests
 app.listen(3000, () =>{ //listen for requests from this port
     console.log('listening on port 3000')
 })
