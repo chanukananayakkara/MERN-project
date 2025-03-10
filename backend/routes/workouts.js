c330d95b5451e6a02724eea2d598216b7a12eb78
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-03-11 01:11:41 +0530

    created router object using express router to access express app

---
+++ backend/routes/workouts.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const router = express.Router() //used for access the express app in this file
+
+router.get('/', () => {}) //the above Router method creates a router object to access express app in this file
+
+module.exports = router
