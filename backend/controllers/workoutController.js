c5a3c60e0ccc8f1f09a66ed246fb165aee861209
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-03-11 23:53:30 +0530

    created the function that occuring in the post method in a seperate controller file

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,39 @@
+//functions created in this file will called in workouts.js routes file
+
+const Workout = require('../models/workoutModel') //We mention this file in here because we need this file to work with the database
+
+// get all workouts
+
+
+
+//get a single workout
+
+
+
+//create new workout
+const createWorkout = async (req, res) =>{ //createWorkout is a object to access the function and this is a async function with req and res parameters
+
+    const {title, load, reps} = req.body //this means desctructure the body of the POST message and and take the fields specified by accessing the req property.
+
+
+    //add document(workout) to db
+    try {
+        const workout = await Workout.create({title, load, reps}) //create a new workout by calling the  create method on workoutModel in the collection
+        res.status(200).json(workout) //return the created object using a Json file
+    } catch (error) {
+        res.status(400).json({error:error.message}) //if the creation is not succeed, then return the error as a json object
+        
+    }
+}
+
+
+//delete a workout
+
+
+
+//update a workout
+
+module.exports = { //in here we can export the functions we created in this controller file.
+    createWorkout
+
+}
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const Workout = require('../models/workoutModel') //workoutModel is required to create a new workout in the collection
-
+ 
 const router = express.Router() //used for access the express app in this file
 //the above Router method creates a router object to access express app in this file
 
@@ -17,18 +16,7 @@ router.get('/:id', (req, res) =>{
 
 
 //POST a new workout
-router.post('/', async (req, res) =>{
-    const {title, load, reps} = req.body //this means desctructure the body of the POST message and and take the fields specified by accessing the req property.
-
-    try {
-        const workout = await Workout.create({title, load, reps}) //create a new workout by calling the  create method on workoutModel in the collection
-        res.status(200).json(workout) //return the created object using a Json file
-    } catch (error) {
-        res.status(400).json({error:error.message}) //if the creation is not succeed, then return the error as a json object
-        
-    }
-    
-})
+router.post('/', createWorkout) //calling the function created in the controller file when occuring the POST request
 
 
 //DELETE a workout
