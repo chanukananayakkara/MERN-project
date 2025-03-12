b6395e62d7e86f598d3bfcedaf994ebbdbfc9e9e
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-03-13 04:23:38 +0530

    mention the purpose of using the BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+//BrowserRouter wraps the everywhere we use the router
+//Routes component wraps all the individual wraps
+//Route component uses to create a single route
+
 function App() {
   return (
     <div className="App">
+
+      
+      <BrowserRouter>
+        {/*Usage of the BrowserRouter is to surrounds everything that needs the browsing system*/}
+      
+      </BrowserRouter>
       
     </div>
   );
