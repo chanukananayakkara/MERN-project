1211f45c33912dccc9b98a46637f32613f07dbb0
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-03-13 17:55:19 +0530

    created a new component to provide a better template to a workout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react"
 
+//components
+import WorkoutDetails from '../components/WorkoutDetails'
+
+
 const Home = () =>{
     const [workouts, setWorkouts] = useState(null)
 
