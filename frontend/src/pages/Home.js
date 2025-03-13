939c05147ff573e242d5d29f2596dcea27277203
Chanuka Nanayakkara <155652752+chanukananayakkara@users.noreply.github.com>
2025-03-13 14:35:26 +0530

    used the useEffect hook in the Home.js file to fetch the data from the API and display those data only when the Home component renders

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react"
+
 const Home = () =>{
+
+    useEffect(()=>{
+        //used to fetch data through the API when the UI appears/component renders. It fired the function mentioned inside when the component renders
+    }, []) //[] is the dependency array and we keep it empty to call the function only the component is rendered.
     return (
         <div className="home">
             <h2>Home</h2>
